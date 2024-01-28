d860acb69e835f0d211eff64fe349bdf9898a68e
Daniil Marukhlenko <daniil.marukh@gmail.com>
2024-01-28 18:52:35 +0300

    clean code

--- examples/write_bytes/main/include/main.h
+++ examples/write_bytes/main/include/main.h
@@ -2,12 +2,7 @@
 
 #include "i2c.h"
 
-#include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <math.h>
 #include <esp_log.h>
 
 #define WRITE_BYTES_MAIN_H
-
 #endif //WRITE_BYTES_MAIN_H
--- include/i2c.h
+++ include/i2c.h
@@ -1,5 +1,5 @@
-#ifndef JAPERSIK_ESP32_I2C_LCD_H
-#define JAPERSIK_ESP32_I2C_LCD_H
+#ifndef JAPERSIK_ESP32_I2C_DEVICE_H
+#define JAPERSIK_ESP32_I2C_DEVICE_H
 #include "driver/i2c.h"
 
 /**
@@ -18,4 +18,4 @@ typedef struct {
 esp_err_t i2c_device_init(i2c_device_info_t * i2c_device_info, i2c_port_t i2c_port, i2c_address_t address);
 esp_err_t i2c_device_send_byte(const i2c_device_info_t * i2c_device_info, uint8_t data);
 
-#endif //JAPERSIK_ESP32_I2C_LCD_H
+#endif //JAPERSIK_ESP32_I2C_DEVICE_H
